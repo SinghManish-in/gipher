7216f536630e57dd168a6c2346dbbeeff38d0846
Singh <437038@cognizant.com>
2019-07-20 15:48:03 +0530

    cts laptop

--- giphermanager/src/main/java/com/stackroute/giphermanager/controller/GipherController.java
+++ giphermanager/src/main/java/com/stackroute/giphermanager/controller/GipherController.java
@@ -1,14 +1,11 @@
 package com.stackroute.giphermanager.controller;
 
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
