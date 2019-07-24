39e24441739b3ea72dcbea95d8c0ba6e2c873ae8
Singh <437038@cognizant.com>
2019-07-24 16:04:55 +0530

    JWT implemented in both services

--- accountmanager/src/main/java/com/stackroute/accountmanager/controller/UserAuthenticationController.java
+++ accountmanager/src/main/java/com/stackroute/accountmanager/controller/UserAuthenticationController.java
@@ -8,7 +8,6 @@ import java.util.HashMap;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -26,25 +25,11 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
-/*
- * As in this assignment, we are working on creating RESTful web service, hence annotate
- * the class with @RestController annotation. A class annotated with the @Controller annotation
- * has handler methods which return a view. However, if we use @ResponseBody annotation along
- * with @Controller annotation, it will return the data directly in a serialized 
- * format. Starting from Spring 4 and above, we can use @RestController annotation which 
- * is equivalent to using @Controller and @ResposeBody annotation
- */
 @RestController
 @CrossOrigin(origins="*")
 public class UserAuthenticationController {
 	
 	HashMap<String, String> map = new HashMap<>();
-
-    /*
-	 * Autowiring should be implemented for the UserAuthenticationService. (Use Constructor-based
-	 * autowiring) Please note that we should not create an object using the new
-	 * keyword
-	 */
 
 	@Autowired
 	UserAuthenticationService authicationService;
@@ -52,16 +37,6 @@ public class UserAuthenticationController {
     	this.authicationService = authicationService;
 	}
 
-/*
-	 * Define a handler method which will create a specific user by reading the
-	 * Serialized object from request body and save the user details in the
-	 * database. This handler method should return any one of the status messages
-	 * basis on different situations:
-	 * 1. 201(CREATED) - If the user created successfully. 
-	 * 2. 409(CONFLICT) - If the userId conflicts with any existing user
-	 * 
-	 * This handler method should map to the URL "/api/v1/auth/register" using HTTP POST method
-	 */
     @PostMapping("/api/v1/auth/register")
     public ResponseEntity<?> registerUser(@RequestBody User user){
     	System.out.println(user);
@@ -79,24 +54,9 @@ public class UserAuthenticationController {
     	
     }
 
-
-	/* Define a handler method which will authenticate a user by reading the Serialized user
-	 * object from request body containing the username and password. The username and password should be validated 
-	 * before proceeding ahead with JWT token generation. The user credentials will be validated against the database entries. 
-	 * The error should be return if validation is not successful. If credentials are validated successfully, then JWT
-	 * token will be generated. The token should be returned back to the caller along with the API response.
-	 * This handler method should return any one of the status messages basis on different
-	 * situations:
-	 * 1. 200(OK) - If login is successful
-	 * 2. 401(UNAUTHORIZED) - If login is not successful
-	 * 
-	 * This handler method should map to the URL "/api/v1/auth/login" using HTTP POST method
-	*/
-
-
     @PostMapping("/api/v1/auth/login")
     public ResponseEntity<?> login(@RequestBody User user){
-    	System.out.println(user.getUserId()+""+ user.getUserPassword());
+    	System.out.println(user.getUserId()+"-"+ user.getUserPassword());
     	try {
 			String token = getToken(user.getUserId(), user.getUserPassword());
 			map.clear();
@@ -104,7 +64,6 @@ public class UserAuthenticationController {
 			map.put("message", "User logged in successfully");
 			
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 			map.clear();
 			map.put("token", null);
@@ -132,14 +91,10 @@ public class UserAuthenticationController {
     	map.clear();
     	map.put("isAuthenticated", true);
     	return new ResponseEntity<>(map,HttpStatus.OK);
-    	
     }
-
-
 
     // Generate JWT token
 	public String getToken(String username, String password) throws Exception {
-			
 		if(username == null || password == null) {
 			
 		}
